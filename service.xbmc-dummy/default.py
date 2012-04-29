446f619043ee1d7595ae2e6a7d329c0f3f8d973f
Frank Epperlein <mail@shellshark.de>
2012-04-29 21:08:25 +0200

    successfully changed the dummy to handle Calls asynchronous

--- service.xbmc-dummy/default.py
+++ service.xbmc-dummy/default.py
@@ -1,7 +1,7 @@
 import xbmc, xbmcaddon
 import os, time
 import socket
-import threading
+import service, asyncore
 
 # Script constants
 __addon__       = "Dummy"
@@ -13,8 +13,11 @@ xbmc.log("DUMMY Started")
  
 while 1:
     print "DUMMY Abort Requested: "+str(xbmc.abortRequested)
+    client = service.FritzClient('192.168.178.1', 1012)
+    asyncore.loop()
     if (xbmc.abortRequested):
         xbmc.log("DUMMY Aborting...")
+        client.handle_close()
         break
     time.sleep(1)
  
---
+++ service.xbmc-dummy/service.py
@@ -0,0 +1,28 @@
+import asyncore, socket, xbmc
+
+class FritzClient(asyncore.dispatcher):
+
+    def __init__(self, host,port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect( (host, port) )
+
+    def handle_connect(self):
+        pass
+
+    def handle_close(self):
+        self.close()
+
+    def handle_read(self):
+        antwort = self.recv(1024) 
+        log= "[%s] %s" % ('192.168.178.1',antwort)
+        xbmc.log(log)
+
+    def writable(self):
+        pass
+
+    def handle_write(self):
+        pass
+
+
+
