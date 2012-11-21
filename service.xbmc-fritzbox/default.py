25f6e1524ddcd0b6e2959edbafeac4bd77961c12
Frank Epperlein <mail@shellshark.de>
2012-11-21 14:24:54 +0100

    aufloesen der rufnummern ueber PytzBox

--- service.xbmc-fritzbox/default.py
+++ service.xbmc-fritzbox/default.py
@@ -1,21 +1,9 @@
-# Open Source Initiative OSI - The MIT License (MIT):Licensing
-#[OSI Approved License]
-#The MIT License (MIT)
-
-#Copyright (c) 2011 N.K.
-#Copyright (c) 2012 FEP
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
 from pprint import pformat
 import xbmc, xbmcaddon
 import socket
 import os
-import xml.sax
+from lib.PytzBox import PytzBox
+
 
 # Script constants
 __addon__       = xbmcaddon.Addon()
@@ -25,85 +13,63 @@ __url__         = "http://code.google.com/p/xbmc-fritzbox"
 __version__     = "1"
 
 
-class FbAbHandler(xml.sax.ContentHandler):
-
-    def __init__(self,tele):
-        self.contactname=""
-        self.aktiv=None
-        self.telefonbuch = tele
-
-    def startElement(self,  name,  attrs):
-        if name == "contact":
-            self.contactname =""
-        elif name == "realName" or name == "number":
-            self.aktiv = name
-
-    def endElement (self,  name):
-        if name == "realName" or name == "number":
-            self.aktiv = None
-
-    def characters(self,  content):
-        if self.aktiv == "realName":
-            self.contactname = content
-        if self.aktiv == "number":
-            content.encode()
-            self.telefonbuch[content] = self.contactname.encode()
-
-
-
-class Fritzboxtelefonbuch():
-    def __init__ (self, xbmctele, url):
-        self.parser = xml.sax.make_parser()
-        self.handler = FbAbHandler(xbmctele)
-        self.parser.setContentHandler(self.handler)
-        try:
-            self.parser.parse(open(url, "r"))
-        except IOError:
-            print "Datei %s konnte nicht gefunden werden" % url
-        except Exception, msg:
-            print "Fehler %s aufgetreten" % msg
-
-
-    def getTelefonbuch(self):
-        return self.handler.telefonbuch
-
-
 
 class FritzCallmonitor():
+
+    __pytzbox = None
+    __fb_phonebook = None
 
     def error(*args, **kwargs):
         xbmc.log("ERROR: %s %s" % (args, kwargs))
 
+    def getNameByNumber(self, request_number):
+
+        if __addon__.getSetting( "AB_Fritzadress" ) == 'true':
+
+            if self.__pytzbox is None:
+
+                password = False
+                if __addon__.getSetting( "AB_FritzboxPassword" ) and len(str(__addon__.getSetting( "AB_FritzboxPassword" ))) > 0:
+                    password = __addon__.getSetting( "AB_FritzboxPassword" )
+
+                self.__pytzbox = PytzBox.PytzBox(password=password, host=__addon__.getSetting( "S_IP" ))
+
+                if password:
+                    self.__pytzbox.login()
+
+            if self.__fb_phonebook is None:
+                self.__fb_phonebook = self.__pytzbox.getPhonebook()
+
+            if isinstance(self.__fb_phonebook, dict):
+                for entry in self.__fb_phonebook:
+                    if 'numbers' in self.__fb_phonebook[entry]:
+                        for number in self.__fb_phonebook[entry]['numbers']:
+                            if number.endswith(request_number):
+                                return entry
+
+        return False
+
+
+
     def handleOutgoingCall(self, aList):
         datum, funktion, connectionID, Nebenstelle, GenutzteNummer, AngerufeneNummer, sip,  leer = aList
         xbmc.log(str(aList))
-        self.Notification("Ausgehender Anruf", "zu %s (von %s)" % (AngerufeneNummer, GenutzteNummer))
-
-    def getPicByName(self, name):
-        PicFolder = __addon__.getSetting( "AB_Pics" )
-        PIC = xbmc.translatePath(os.path.join(PicFolder, "%s.png" % name))
-        try: open(PIC).close()
-        except Exception, e:
-            xbmc.log('%s: %s' % (PIC, str(e)))
-            PIC = False
-        return PIC
-
-    def getNameByNumber(self, number):
-        return xbmctelefonbuch.get(number,'Unbenannt')
+        name = self.getNameByNumber(number) or 'Unbekannt'
+        self.Notification("Ausgehender Anruf", "zu %s [%s] (von %s)" % (name, AngerufeneNummer, GenutzteNummer))
 
     def handleIncomingCall(self, aList):
         xbmc.log(str(aList))
         number = aList[3]
-        name = self.getNameByNumber(number)
-        picture = self.getPicByName(name)
-        self.Notification('Eingehender Anruf', 'Von %s [%s]' % (name, number), img=picture)
+        name = self.getNameByNumber(number) or 'Unbekannt'
+        self.Notification('Eingehender Anruf', 'Von %s [%s]' % (name, number))
 
     def handleConnected(self, aList):
         datum, funktion, connectionID, nebenstelle, nummer, leer = aList
         xbmc.log(str(aList))
         if __addon__.getSetting( "AC_Pause" )  == 'true':
             xbmc.Player().pause()
-        self.Notification('Verbindung hergestellt', 'Mit %s' % nummer)
+        name = self.getNameByNumber(number) or 'Unbekannt'
+        self.Notification('Verbindung hergestellt', 'Mit %s [%s]' % (name, nummer))
 
     def handleDisconnected(self, aList):
         datum, funktion, connectionID, dauer,  leer = aList
@@ -153,8 +119,5 @@ class FritzCallmonitor():
             xbmc.log("XBMC-Fritzbox Addon beendet.")
 
 
-xbmctelefonbuch = {}
-if __addon__.getSetting( "AB_Fritzadress" ) == 'true':
-    xbmctelefonbuch = Fritzboxtelefonbuch(xbmctelefonbuch, __addon__.getSetting( "AB_Adressbookpath")).getTelefonbuch()
 
 FritzCallmonitor().start()
