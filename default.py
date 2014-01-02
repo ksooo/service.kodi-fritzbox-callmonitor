7bd14d5d57342f25f090e7e4e7bd8ac9ab207453
Frank Epperlein <mail@shellshark.de>
2014-01-02 12:15:19 +0100

    option to pause all media

--- default.py
+++ default.py
@@ -50,6 +50,7 @@ class FritzCallMonitor():
         self.__fb_phonebook = None
         self.__autopaused = False
         self.__autovolumelowered = False
+        self.__connections = dict()
         self.__ring_time = False
         self.__gdata_request = None
         self.__klicktel_phonebook = None
@@ -105,28 +106,28 @@ class FritzCallMonitor():
             self.command = response[1]
             if self.command == 'CALL':
                 self['date'] = response[0]
-                self['connection_id'] = response[2]
+                self['connection_id'] = int(response[2])
                 self['extension'] = response[3]
-                self['number_used'] = response[4]
+                self['number_caller'] = response[4]
                 self['number_called'] = response[5]
                 self['sip'] = response[6]
 
             elif self.command == 'RING':
                 self['date'] = response[0]
-                self['connection_id'] = response[2]
+                self['connection_id'] = int(response[2])
                 self['number_caller'] = response[3]
                 self['number_called'] = response[4]
                 self['sip'] = response[5]
 
             elif self.command == 'CONNECT':
                 self['date'] = response[0]
-                self['connection_id'] = response[2]
+                self['connection_id'] = int(response[2])
                 self['extension'] = response[3]
                 self['number'] = response[4]
 
             elif self.command == 'DISCONNECT':
                 self['date'] = response[0]
-                self['connectionID'] = response[2]
+                self['connection_id'] = int(response[2])
                 self['duration'] = response[3]
             if 'date' in self:
                 #noinspection PyBroadException
@@ -168,6 +169,17 @@ class FritzCallMonitor():
         b = b[-len(a):]
 
         return a == b
+
+    def is_ignored_number(self, number, printout=False):
+        if not isinstance(number, list):
+            number = [number,]
+        for single_number in number:
+            for ignored_number in re.findall(r'(\d+)', __addon__.getSetting("AB_IgnoreNumbers")):
+                if self.equal_numbers(single_number, ignored_number):
+                    if printout:
+                        print "%s is ignored" % single_number
+                    return single_number
+        return False
 
     def get_name_by_number(self, request_number):
 
@@ -223,14 +235,36 @@ class FritzCallMonitor():
 
         return False
 
+    @staticmethod
+    def is_playback_paused():
+        return bool(xbmc.getCondVisibility("Player.Paused"))
+
+    def resume_playback(self):
+        if self.is_playback_paused():
+            xbmc.Player().pause()
+
     def handle_outgoing_call(self, line):
+
+        if self.is_ignored_number([line.number_caller, line.number_called], printout=True):
+            return False
+        else:
+            self.__connections[line.connection_id] = line
+
         name = self.get_name_by_number(line.number_called) or str(line.number_called)
         image = self.get_image_by_name(name)
-        self.show_notification(_('leaving call'), _('to %s (by %s)') % (name, line.number_used), img=image)
+        self.show_notification(_('leaving call'), _('to %s (by %s)') % (name, line.number_caller), img=image)
         if xbmc.Player().isPlayingVideo():
             self.__ring_time = xbmc.Player().getTime()
 
     def handle_incoming_call(self, line):
+
+        if self.is_ignored_number([line.number_caller, line.number_called], printout=True):
+            return False
+        else:
+            self.__connections[line.connection_id] = line
+
+        pprint.pprint(self.__connections)
+
         name = self.get_name_by_number(line.number_caller) or str(line.number_caller)
         image = self.get_image_by_name(name)
 
@@ -252,6 +286,10 @@ class FritzCallMonitor():
 
 
     def handle_connected(self, line):
+
+        if not line.connection_id in self.__connections:
+            return False
+
         name = self.get_name_by_number(line.number) or str(line.number)
         image = self.get_image_by_name(name)
 
@@ -268,15 +306,11 @@ class FritzCallMonitor():
                     xbmc.Player().seekTime(self.__ring_time)
                     self.__autopaused = True
 
-    @staticmethod
-    def is_playback_paused():
-        return bool(xbmc.getCondVisibility("Player.Paused"))
-
-    def resume_playback(self):
-        if self.is_playback_paused():
-            xbmc.Player().pause()
-
     def handle_disconnected(self, line):
+
+        if not line.connection_id in self.__connections:
+            return False
+
         self.show_notification(_('call ended'), _('duration: %sh') % str(line.duration))
 
         if self.__autovolumelowered:
@@ -288,6 +322,8 @@ class FritzCallMonitor():
             if __addon__.getSetting("AC_Resume") == 'true':
                 self.resume_playback()
             self.__autopaused = False
+
+        del self.__connections[line.connection_id]
 
     @staticmethod
     def show_notification(title, text, duration=False, img=False):
